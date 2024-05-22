546d1fb06ae617789cfd476fc6e37afd01d91f5e
Rishi Singh <rs3949427@gmail.com>
2024-05-22 21:10:58 +0530

    migrate and generate the prisma client and added env example

---
+++ prisma/migrations/20240522153958_created_contacts_schema/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "LinkPrecedence" AS ENUM ('primary', 'secondary');
+
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" SERIAL NOT NULL,
+    "phoneNumber" TEXT,
+    "email" TEXT,
+    "linkedId" INTEGER,
+    "linkPrecedence" "LinkPrecedence" NOT NULL DEFAULT 'primary',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "email_index" ON "Contact"("email");
+
+-- CreateIndex
+CREATE INDEX "phone_number_index" ON "Contact"("phoneNumber");
